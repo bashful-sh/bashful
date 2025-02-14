e52924c9d742160e2561da1998325518a8e3722a
Owen Easter <owen@easter.company>
2025-02-14 10:03:27 +0000

    fix: downgrade python to 3.12

--- python/install.sh
+++ python/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 venv="$BASHFUL_DIR/.tmp/pyvenv"
+
 if ! [ -d "$venv" ]; then
-  sudo apt install --upgrade python3.13 python3-venv
-  python3.13 -m venv "$venv"
+  sudo apt install --upgrade python3 python3-venv python3-dev
+  python3 -m venv "$venv"
   export BPY_VENV="$BASHFUL_DIR/.tmp/pyvenv"
   export BPY_BIN="$BPY_VENV/bin"
   export BPY="$BPY_BIN/python3"
