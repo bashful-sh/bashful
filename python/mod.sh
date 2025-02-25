72567a2e3da9e4ada8c69ced31ce21bd614b1d78
Owen Easter <owen@easter.company>
2025-02-25 22:43:53 +0000

    fix: consistency fix

--- python/mod.sh
+++ python/mod.sh
@@ -11,9 +11,9 @@ export BPY_BIN="$BPY_VENV/bin"
 export BPY="$BPY_BIN/python3"
 export BPIP="$BPY_BIN/python3 -m pip"
 export BVENV="$BPY_BIN/activate"
-export LD_LIBRARY_PATH
 
 LD_LIBRARY_PATH=$("$BPY" -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))')
+export LD_LIBRARY_PATH
 
 # Bashful Python
 function bpy() {
