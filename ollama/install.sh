05aeab936fffafae8583b4a962c0ebe63e5b4db6
Owen Easter <owen@easter.company>
2025-02-25 22:43:32 +0000

    add: auto install ollama

--- ollama/install.sh
+++ ollama/install.sh
@@ -4,6 +4,10 @@
 # Installs any requirements & dependencies for using
 # the bashful ollama module.
 
-# Version information
+if ! command -v ollama &>/dev/null; then
+  echo "Ollama is not installed. Installing..."
+  curl -fsSL https://ollama.com/install.sh | sh
+fi
+
 OLLAMA_VERSION="$(ollama -v | awk '{print $4}')"
 export OLLAMA_VERSION
