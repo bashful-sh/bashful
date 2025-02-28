0e7e9803abc9341c06189d7e2606610ee6525b92
Owen Easter <owen@easter.company>
2025-02-28 06:09:40 +0000

    add: combined llm web service

--- python/libs/llm/main.py
+++ python/libs/llm/main.py
@@ -1,11 +1,35 @@
+import redis
+import pickle
+import whisper
 import argparse
+import tempfile
 import requests
 
-from flask import Flask, request, jsonify
+from google.cloud import texttospeech
+from flask import Flask, request, jsonify, Response
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app)
+
+tts_client = texttospeech.TextToSpeechClient()
+model_cache = {}
+redis_client = None
+
+
+def tts(text: str, language: str = "en-GB", voice: str = "en-GB-Chirp-HD-D"):
+    input_text = texttospeech.SynthesisInput(text=text)
+    voice = texttospeech.VoiceSelectionParams(
+        language_code=language,
+        name=voice,
+    )
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.LINEAR16, speaking_rate=1
+    )
+    response = tts_client.synthesize_speech(
+        request={"input": input_text, "voice": voice, "audio_config": audio_config}
+    )
+    return response.audio_content
 
 
 def generate_chat_response(chat_history, model="dexter-0.5b") -> str:
@@ -42,6 +66,48 @@ def generate_chat_response(chat_history, model="dexter-0.5b") -> str:
         return str(e)
 
 
+def load_stt_model(size="tiny.en"):
+    model = None
+
+    if redis_client is not None:
+        try:
+            redis_client.ping()
+            cached_model = redis_client.get(f"stt_model:{size}")
+            if cached_model:
+                model = pickle.loads(cached_model)
+            else:
+                model = whisper.load_model(size)
+                redis_client.set(
+                    f"stt_model:{size}",
+                    pickle.dumps(model),
+                    ex=(
+                        86400 * 7
+                    ),  # Expire cached model after 7 days. (will also cause the model to update)
+                )
+            return model
+        except redis.exceptions.ConnectionError as e:
+            print(f"Redis connection error: {e}")
+
+    if size not in model_cache:
+        model_cache[size] = whisper.load_model(size)
+
+    return model_cache[size]
+
+
+@app.route("/tts", methods=["POST"])
+@cross_origin()
+def tts_api_endpoint():
+    try:
+        request_data = request.get_json()
+        if not request_data or "text" not in request_data:
+            return jsonify({"error": "Invalid request data"}), 400
+        text = request_data["text"]
+        audio_data = tts(text)
+        return Response(audio_data, mimetype="audio/l16")
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
 @app.route("/llm", methods=["POST"])
 @cross_origin()
 def prompt_api_endpoint():
@@ -58,13 +124,36 @@ def prompt_api_endpoint():
         return jsonify({"error": str(e)}), 500
 
 
+@app.route("/stt", methods=["POST"])
+@cross_origin()
+def transcription_api_endpoint():
+    if "audio" not in request.files:
+        return jsonify({"error": "No audio file part"}), 400
+
+    audio_file = request.files["audio"]
+
+    if audio_file.filename == "":
+        return jsonify({"error": "No selected audio file"}), 400
+
+    try:
+        model = load_stt_model()
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".wav") as temp:
+            audio_file.save(temp.name)
+            result = model.transcribe(temp.name, language=None)
+        return jsonify(
+            {"text": result["text"].strip(), "language": result["language"].strip()}
+        ), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--port",
         "-p",
         type=int,
-        default=9501,
+        default=9500,
         help="Which port to run the server on.",
     )
     parser.add_argument(
@@ -75,11 +164,35 @@ if __name__ == "__main__":
         help="Which mode to run the server in.",
     )
     parser.add_argument(
-        "--prompt", "-t", type=str, default="", help="Inference the llm directly."
+        "--chat", "-c", type=str, default="", help="Inference the llm directly."
+    )
+    parser.add_argument(
+        "--use_redis",
+        "-r",
+        type=bool,
+        default=False,
+        help="Which caching mechanism to use.",
+    )
+    parser.add_argument(
+        "--redis_host",
+        type=str,
+        default="localhost",
+        help="Redis host parameter.",
+    )
+    parser.add_argument(
+        "--redis_port",
+        type=int,
+        default=6379,
+        help="Redis port parameter.",
     )
     args = parser.parse_args()
+
+    if args.use_redis or not args.debug:
+        redis_client = redis.Redis(host=args.redis_host, port=args.redis_port, db=0)
+
     if len(args.prompt) == 0:
         app.run(debug=args.debug, host="127.0.0.1", port=args.port)
+
     else:
         r = generate_chat_response([{"role": "user", "content": args.prompt}])
         print(r)
